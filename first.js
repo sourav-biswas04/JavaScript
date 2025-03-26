bdbbdcf4dc6aacc612b8c2559de8cc39e654f8e9
sourav-biswas04 <sb303180@gmail.com>
2025-03-26 10:05:02 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,11 @@
+let student={
+    name: 'John',
+    age: 25,
+    course: 'IT'
+};
+// student["age"]=student["age"]+1;
+// console.log(student["age"]);
+
+student["name"]="Sourav Biswas";
+console.log(student["name"]);
+console.log(student);
