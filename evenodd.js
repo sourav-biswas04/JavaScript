62ec9ef4d7366b692c4017b9de2ffebe77f82f86
sourav-biswas04 <sb303180@gmail.com>
2025-03-26 10:52:00 +0530

    Even odd programe

---
+++ evenodd.js
@@ -0,0 +1,9 @@
+let num=prompt("Enter a number");
+if(num%2===0){
+    console.log("even");
+    
+}
+else{
+    console.log("odd");
+    
+}
--- oper.js
+++ oper.js
@@ -11,6 +11,6 @@
 
 // NOT operator
 
-let a=5;
-let b=6;
-console.log("!(a>b)=",!(a>b));
+// let a=5;
+// let b=6;
+// console.log("!(a>b)=",!(a>b));
