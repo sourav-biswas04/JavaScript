302a64bb14f3aee389e4153d76b2f743e995bca9
sourav-biswas04 <sb303180@gmail.com>
2025-03-26 10:42:53 +0530

    OR operator

--- oper.js
+++ oper.js
@@ -1,4 +1,9 @@
+// AND operator
+// let a=5;
+// let b=6;
+// console.log("a && b=",a<b && a>b);
+
+// OR operator
 let a=5;
 let b=6;
-console.log("a && b=",a<b && a>b);
-
+console.log("a || b=",a<b || a==b);
